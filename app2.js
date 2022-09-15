857b808631fa9386393292514b3dbffda2c81859
JMSoria <josenmarie.soria.cics@ust.edu.ph>
2022-09-15 12:03:29 +0800

    added app2.js

--- app2.js
+++ app2.js
@@ -3,12 +3,22 @@
   
 // Accessing fs module
 const fs = require("fs");
- 
+
+const path = require('path');
+
+fs.mkdir(path.join(__dirname,'data'),
+{recursive:true},(err) =>{
+    if(err){
+        return console.error(err);
+    }
+    console.log("Directory created successfully")
+})
+
 // Create a readable stream
 const readable = fs.createReadStream('info.txt');
  
 // Create a writable stream
-const writable = fs.createWriteStream('info2.txt');
+const writable = fs.createWriteStream('data/info2.txt');
  
 // Calling pipe method
 readable.pipe(writable);
