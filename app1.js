e1358e4e7d115a536af12daca6a2ce947553409e
JMSoria <josenmarie.soria.cics@ust.edu.ph>
2022-09-15 09:52:06 +0800

    initial project

---
+++ app1.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+let content = fs.readFile ('./info.txt','utf-8',(error,data)=>{
+    if (!error)console.log(data)
+    else console.log('error reading the file...')
+})
+
+console.log('This line comes after reading the file.....')
